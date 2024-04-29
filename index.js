82a8e5a2043a35d79b5378a4d416840693c8bab4
rafaulgoni <rafaulgoni787898@gmail.com>
2024-04-29 08:36:25 +0600

    done server site

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-
 //middleware
 app.use(cors())
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtozrqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,28 +22,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const artCollection = client.db('artDB').collection('art');
     const userCollection = client.db('artDB').collection('user');
-
 
     app.get('/art', async (req, res) => {
       const cursor = artCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
+
     app.get('/art/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await artCollection.findOne(query);
       res.send(result);
-    })
+    });
+
     app.post('/art', async (req, res) => {
       const newArt = req.body;
       const result = await artCollection.insertOne(newArt);
       res.send(result);
-    })
+    });
 
     //All Craft Items
 
@@ -55,19 +52,19 @@ async function run() {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     app.get('/user/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.findOne(query);
       res.send(result);
-    })
+    });
 
     app.get('/data/:email', async (req, res) => {
       const result = await userCollection.find({email: req.params.email}).toArray();
       res.send(result);
-    })
+    });
 
     app.post('/user', async (req, res) => {
       const user = req.body;
@@ -96,16 +93,17 @@ async function run() {
       }
       const result = await userCollection.updateOne(filter, Craft, options);
       res.send(result);
-  })
+  });
 
     app.delete('/user/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
       res.send(result);
-  })
+  });
+  
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
